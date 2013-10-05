7a1f02d60f5a7250abba5827bc8bfd7929dcb8f6
karlzheng <zhengkarl@gmail.com>
2013-10-05 17:52:26 +0800

    init

---
+++ bin/ringdroid-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/media/work/source/lyric/myringdroid/bin/ringdroid-debug-unaligned.apk : \
+/media/work/source/lyric/myringdroid/bin/ringdroid.ap_ \
+/media/work/source/lyric/myringdroid/bin/classes.dex \
+/media/cdriver/work/software/android-sdk-linux_86/tools/support/annotations.jar \
