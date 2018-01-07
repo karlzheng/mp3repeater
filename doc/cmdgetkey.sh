859225dc0d619ecb58c8c7a93421e276bf50cf03
KarlZheng <zhengkl@yuzhou.com>
2018-01-07 13:24:04 +0800

    new file:   doc/key.txt
    Signed-off-by: KarlZheng <zhengkl@yuzhou.com>

---
+++ doc/cmdgetkey.sh
@@ -0,0 +1,6 @@
+#/Users/karlzheng/Library/Android/sdk/platform-tools/adb shell << EEOOFF
+##getevent -l /dev/input/event8
+#getevent /dev/input/event8
+#EEOOFF
+
+/Users/karlzheng/Library/Android/sdk/platform-tools/adb shell getevent
